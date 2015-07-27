ca40f228addf6e058a2869003cc4577fabb1e767
Lolleko <larry.junglas@hotmail.de>
2015-07-27 21:54:09 +0200

    Hotfix

--- entities/weapons/weapon_destroy.lua
+++ entities/weapons/weapon_destroy.lua
@@ -25,7 +25,7 @@ SWEP.WorldModel = ""
 
 function SWEP:PrimaryAttack()
 
-   self.Weapon:SetNextPrimaryFire( CurTime() + 1 )
+   self.Weapon:SetNextPrimaryFire( CurTime() + 3 )
 
    	if self.Owner.LagCompensation then
 		self.Owner:LagCompensation(true)
@@ -35,12 +35,16 @@ function SWEP:PrimaryAttack()
     local tr = util.TraceLine({start=spos, endpos=sdest, filter=self.Owner, mask=MASK_SHOT_HULL})
     self.Owner:LagCompensation(false)
 
-    print(tr.Entity)
+    --print(tr.Entity)
 
     if tr.Hit and IsValid(tr.Entity) and (tr.Entity:GetClass() == "func_breakable" or tr.Entity:GetClass() == "func_breakable_surf") then
 
-    	tr.Entity:Fire("Shatter")
-
+    	if SERVER then tr.Entity:Fire("shatter") end
+    	
     end
+
+end
+
+function SWEP:SecondaryAttack()
 
 end
