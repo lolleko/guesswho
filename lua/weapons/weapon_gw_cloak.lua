35164b1150c04bd27b6f9bc2e3455adf56b945c1
Lolleko <larry.junglas@hotmail.de>
2015-12-27 10:25:40 +0100

    hotfix (again)

--- lua/weapons/weapon_gw_cloak.lua
+++ lua/weapons/weapon_gw_cloak.lua
@@ -5,12 +5,13 @@ function SWEP:Ability()
     local ply = self.Owner
     timer.Create( "Ability.Effect." .. ply:SteamID(), 5, 1, function() self:OnRemove() end )
     ply:SetRenderMode( RENDERMODE_TRANSALPHA )
-    ply:Fire( "alpha", 15, 0 )
+    if SERVER then ply:Fire( "alpha", 15, 0 ) end
 end
 
 function SWEP:OnRemove()
     if !IsValid( self.Owner ) then return end
-    timer.Remove( "Ability.Effect." .. self.Owner:SteamID() )
+    local ply = self.Owner
+    timer.Remove( "Ability.Effect." .. ply:SteamID() )
     ply:SetRenderMode( RENDERMODE_NORMAL )
-    ply:Fire( "alpha", 255, 0 )
+    if SERVER then ply:Fire( "alpha", 255, 0 ) end
 end
