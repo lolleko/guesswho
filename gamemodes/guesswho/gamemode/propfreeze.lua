4e943d7220e962d1a7b1b6dde688719fa55d437f
Shadowsun™ <FarukGamer@users.noreply.github.com>
2016-10-04 20:38:26 +0200

    Changed recommend time

--- gamemodes/guesswho/gamemode/propfreeze.lua
+++ gamemodes/guesswho/gamemode/propfreeze.lua
@@ -1,8 +1,7 @@
 if SERVER then
 	CreateConVar( "gw_propfreeze_enabled", "1", { FCVAR_REPLICATED, FCVAR_ARCHIVE } )
 
-	local Delay = 5 -- between 5-10 is recommended
-
+	local Delay = 5	-- between 5-7 is recommended
 	function DoPropFreeze(ply)
 		timer.Simple( Delay, function()
 			for k, v in pairs( ents.FindByClass( "prop_*" ) ) do 
