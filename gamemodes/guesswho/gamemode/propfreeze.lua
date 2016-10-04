bcd861146c0763a8905f2b3aee4b414ee5cdbc93
Shadowsun™ <FarukGamer@users.noreply.github.com>
2016-10-04 20:14:56 +0200

    Bug fixes and optimizations
    - guesswho.txt is encoded as UTF-8 without BOM (KeyValues Error:
    LoadFromBuffer)
    - calling delay before loop
    - removed 'Instant' feature

--- gamemodes/guesswho/gamemode/propfreeze.lua
+++ gamemodes/guesswho/gamemode/propfreeze.lua
@@ -1,32 +1,22 @@
 if SERVER then
 	CreateConVar( "gw_propfreeze_enabled", "1", { FCVAR_REPLICATED, FCVAR_ARCHIVE } )
 
-	local IsTimed = true -- false makes instant freeze
 	local Delay = 5 -- between 5-10 is recommended
 
 	function DoPropFreeze(ply)
-		for k, v in pairs( ents.FindByClass( "prop_*" ) ) do 
-			local phys = v:GetPhysicsObject()
-			if IsTimed == true and IsValid(phys) then
-				timer.Simple( Delay, function() if (IsValid(phys)) then phys:EnableMotion(false) end end )
-			elseif IsTimed == false and IsValid(phys) then
-				phys:EnableMotion(false)
+		timer.Simple( Delay, function()
+			for k, v in pairs( ents.FindByClass( "prop_*" ) ) do 
+				local phys = v:GetPhysicsObject()
+				if IsValid(phys) then
+					phys:EnableMotion(false)
+				end
 			end
-		end
-
-		local typemsg = "Freeze type set to"
-		if IsTimed == true then
-			print("[INTO] "..typemsg.." 'Timed'")
-		elseif IsTimed == false then
-			print("[INFO] "..typemsg.." 'Instant'")
-		end
-
+		end)
 		print("[INFO] Props are frozen now!")
 	end
 
-	if GetConvar("gw_propfreeze_enabled"):GetBool() then
+	if GetConVar("gw_propfreeze_enabled"):GetBool() then
 		hook.Add( "InitPostEntity", "PropFreezeOnInit", DoPropFreeze )
 		hook.Add( "PostCleanupMap", "PropFreezeOnCleanUP", DoPropFreeze )
 	end
-
 end
