8ec849a9b5163343614eab81b30e3c147dc82763
Blueberry <igory.ygr200@gmail.com>
2019-11-20 15:49:44 +0500

    Create ru.lua

---
+++ gamemodes/guesswho/gamemode/lang/ru.lua
@@ -0,0 +1,47 @@
+local ru = {
+    --Language label
+    language_name = "Русский (Russian)",
+
+    --Round states
+    round_pre_game = "Подготовка игры",
+    round_waiting_players = "Ожидание игроков",
+    round_creating = "Создание NPC",
+    round_hide = "Прячься",
+    round_seek = "Ищи",
+    round_post = "Следующий раунд скоро",
+    round_nav_gen = "Генерация Navmesh",
+
+    --Team Names
+    team_hiding = "Прячущиеся",
+    team_seeking = "Ищущие",
+
+    --Team selection
+    --Links
+    teamselect_workshop_ref = "Страница в Мастерской",
+    teamselect_workshop_changelog = "Список изменений",
+    teamselect_workshop_bug = "Сообщить о ошибке",
+    teamselect_workshop_contact = "Связаться",
+    teamselect_discord = "Discord",
+    --Controls ( limited space keep it short )
+    teamselect_controls_sit = "Сесть",
+    teamselect_controls_ability = "Способность (Прячущиеся)",
+    teamselect_controls_settings = "Помощь + Настройки",
+    teamselect_controls_team = "Выбрать команду",
+    teamselect_controls_random = "Случайная голосовая насмешка",
+    teamselect_controls_taunts = "Жесты",
+    --Buttons
+    teamselect_buttons_spectate = "Наблюдать",
+    teamselect_buttons_auto = "Случайный выбор команды",
+
+    --Scoreboard
+    scoreboard_server = "Сервер",
+    scoreboard_map = "Карта",
+    scoreboard_online = "Онлайн",
+    scoreboard_spectators = "Наблюдатели",
+
+    --HUD
+    hud_ability_pickup = "Новая способность получена",
+
+}
+
+gwlang:addLangguage( ru, "ru" )
