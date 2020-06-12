e3c13502591af90f6a4c8fda8cb76ac55c6f21bd
lolleko <lorenz.junglas@student.kit.edu>
2020-06-12 22:13:58 +0200

    Updated shrink to new weapon base

--- gamemodes/guesswho/entities/weapons/weapon_gw_shrink.lua
+++ gamemodes/guesswho/entities/weapons/weapon_gw_shrink.lua
@@ -3,18 +3,22 @@ AddCSLuaFile()
 SWEP.Base = "weapon_gwbase"
 SWEP.Name = "Shrink"
 SWEP.AbilitySound = "gwabilities/smb2_shrink.wav"
+SWEP.AbilityDuration = 7
+SWEP.AbilityDamagePercentageString = "60%"
+SWEP.AbilityDamageSpeedBonusString = "twice"
+
+SWEP.AbilityDescription = "Shrinks you and your health by $AbilityDamagePercentageString for $AbilityDuration seconds.\n\nFor some reason you are also $AbilityDamageSpeedBonusString as fast while shrunk."
 
 function SWEP:Ability()
-    timer.Create( "Ability.Effect.Shrink" .. self.Owner:SteamID(), 7, 1, function() self:OnRemove() end )
+    self:AbilityTimerIfValidOwner(self.AbilityDuration, 1, true, function() self:AbilityCleanup() end )
     self.Owner:SetRunSpeed(self.Owner:GetRunSpeed() * 1.75)
     self.Owner:SetWalkSpeed(self.Owner:GetWalkSpeed() * 1.75)
     self.Owner:SetModelScale(self.Owner:GetModelScale() / 2.5, 1)
     self.Owner:SetHealth(self.Owner:Health() / 2.5)
 end
 
-function SWEP:OnRemove()
-    if not IsValid( self.Owner ) then return end
-    timer.Remove("Ability.Effect.Shrink" .. self.Owner:SteamID())
+function SWEP:AbilityCleanup()
+    if not IsValid(self.Owner) then return end
     self.Owner:SetRunSpeed(GetConVar("gw_hiding_run_speed"):GetFloat())
     self.Owner:SetWalkSpeed(GetConVar("gw_hiding_walk_speed"):GetFloat())
     self.Owner:SetModelScale(1, 1)
