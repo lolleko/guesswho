9fe9607a1104b858a8bcd07d682d222f9c98bd3a
lolleko <lorenz.junglas@student.kit.edu>
2020-06-12 20:33:06 +0200

    Updated solarflare to new weapon base
    Slightly reduced duration
    Effect radius now scales correctly with cast time

--- gamemodes/guesswho/entities/effects/gw_solarflare.lua
+++ gamemodes/guesswho/entities/effects/gw_solarflare.lua
@@ -6,22 +6,23 @@ EFFECT.Mat = Material("models/debug/debugwhite")
 function EFFECT:Init(data)
 
     self.Entity = data:GetEntity()
-    self.Radius = 10
+    self.Radius = 0
     self.FinalRadius = data:GetRadius()
+    self.Duration = data:GetMagnitude()
     self.Center = self.Entity:OBBCenter()
     self:SetPos(self.Entity:GetPos())
-
-    self.EndTime = CurTime() + 1
-
 end
 
 function EFFECT:Think()
 
     self:SetPos(self.Entity:GetPos())
     self.Center = self.Entity:OBBCenter()
-    if self.Radius < self.FinalRadius then self.Radius = self.Radius + 10 end
+    if self.Radius < self.FinalRadius then
+        self.Radius = self.Radius + FrameTime() * (self.FinalRadius / self.Duration)
+        return true
+    end
 
-    return (CurTime() < self.EndTime)
+    return false
 
 end
 
@@ -31,6 +32,7 @@ function EFFECT:Render()
 
     local pos = self:GetPos() + self.Center
 
+    render.SetColorMaterial()
     render.DrawSphere(pos, self.Radius, 20, 20, Color(255, 255, 255, 255))
 
 end
--- gamemodes/guesswho/entities/weapons/weapon_gw_solarflare.lua
+++ gamemodes/guesswho/entities/weapons/weapon_gw_solarflare.lua
@@ -5,21 +5,24 @@ SWEP.Name = "Solarflare"
 SWEP.AbilitySound = "ambient/energy/zap1.wav"
 
 SWEP.AbilityRange = 400
-SWEP.AbilityShowTargetHalos = false
-SWEP.AbilityDuration = 7
+SWEP.AbilityShowTargetHalos = true
+SWEP.AbilityDuration = 6
+SWEP.AbilityCastTime = 1.5
 
 function SWEP:Ability()
 
 	local ply = self.Owner
 
 	local effectdata = EffectData()
-	effectdata:SetEntity( ply )
-	effectdata:SetRadius( self.AbilityRange )
-
+	effectdata:SetEntity(ply)
+	effectdata:SetRadius(self.AbilityRange)
+	effectdata:SetMagnitude(self.AbilityCastTime)
 	util.Effect( "gw_solarflare", effectdata, true, true )
+
 	for _, v in pairs( player.GetAll() ) do
 		if v:GetPos():Distance( ply:GetPos() ) < self.AbilityRange and v:IsSeeking() then
-			timer.Simple(0.25, function()
+			local distanceRatio = v:GetPos():Distance(ply:GetPos()) / self.AbilityRange
+			timer.Simple(distanceRatio * self.AbilityCastTime, function()
 				if IsValid(v) then
 					v:SetNWFloat("gw_ability_solarflare_endtime", CurTime() + self.AbilityDuration)
 					v:SetNWFloat("gw_ability_solarflare_druation", self.AbilityDuration)
@@ -35,7 +38,7 @@ if CLIENT then
 		if endTime > CurTime() then
 			local alpha = 255
 			local durationRemaining = endTime - CurTime()
-			-- leniar fade out
+			-- linear fade out
 			if durationRemaining <= LocalPlayer():GetNWFloat("gw_ability_solarflare_druation") / 2 then
 				alpha = durationRemaining * 110
 			end
