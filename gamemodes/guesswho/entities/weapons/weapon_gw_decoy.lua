57dba25fe8fec4f2875bd9b246c2f6378d637e56
lolleko <lorenz.junglas@student.kit.edu>
2020-06-14 23:33:51 +0200

    Updated decoy to new SWEP base
    Improved decoy spawn detection

--- gamemodes/guesswho/entities/weapons/weapon_gw_decoy.lua
+++ gamemodes/guesswho/entities/weapons/weapon_gw_decoy.lua
@@ -2,66 +2,87 @@ AddCSLuaFile()
 
 SWEP.Base = "weapon_gwbase"
 SWEP.Name = "Decoy"
+
 SWEP.AbilitySound = "vo/canals/matt_goodluck.wav"
+SWEP.AbilityDecoyCountMin = 1
+SWEP.AbilityDecoyCountMax = 11
+SWEP.AbilityDuration = 14
+SWEP.AbilityDescription = "Spawns between $AbilityDecoyCountMin and $AbilityDecoyCountMax NPCs around you and swaps you with one of the spawned NPCs.\nThe decoys disappear afer $AbilityDuration seconds."
 
 function SWEP:Ability()
 
     if CLIENT then return end
 
     local locations = {
-        Vector( 48, 0, 2 ),
-        Vector( -48, 0, 2 ),
-        Vector( 0, 48, 2 ),
-        Vector( 0, -48, 2 ),
-        Vector( 48, 48, 2 ),
-        Vector( 48, -48, 2 ),
-        Vector( -48, 48, 2 ),
-        Vector( -48, -48, 2),
-        Vector( 96, 0, 2 ),
-        Vector( -96, 0, 2 ),
-        Vector( 0, 96, 2 ),
-        Vector( 0, -96, 2 )
+        Vector(40, 0, 2),
+        Vector(-40, 0, 2),
+        Vector(0, 40, 2),
+        Vector(0, -40, 2),
+        Vector(40, 40, 2),
+        Vector(40, -40, 2),
+        Vector(-40, 40, 2),
+        Vector(-40, -40, 2),
+        Vector(80, 0, 2),
+        Vector(-80, 0, 2),
+        Vector(0, 80, 2),
+        Vector(0, -80, 2),
+        Vector(80, 80, 2),
+        Vector(80, -80, 2),
+        Vector(-80, 80, 2),
+        Vector(-80, -80, 2),
     }
 
-    local decoyCount = 0
+    -- shuffle locations table
+    local rand = math.random
+    local n = #locations
+
+    while n > 2 do
+        local k = rand(1, n) -- 1 <= k <= n
+        locations[n], locations[k] = locations[k], locations[n]
+        n = n - 1
+    end
 
     local walkers = {}
 
-    for _,v in pairs( locations ) do
-        if decoyCount == math.random( 1, 6 ) then break end
+    local decoyCount = math.random(self.AbilityDecoyCountMin, self.AbilityDecoyCountMax)
 
-        local location = self.Owner:GetPos() + v
+    local spawnedCount = 0
 
-        local tr = util.TraceHull( {
+    for _,v in pairs(locations) do
+        if decoyCount == spawnedCount then break end
+
+        local location = self.Owner:GetPos() + v + Vector(0, 0, 8)
+
+        local tr = util.TraceHull({
             start = location,
             endpos = location,
-            maxs = Vector( 16, 16, 70 ),
-            mins = Vector( -16, -16, 0 ),
-        } )
+            maxs = Vector(16, 16, 70),
+            mins = Vector(-16, -16, 0),
+        })
 
-        --debugoverlay.Box( location, Vector( -16, -16, 0 ), Vector( 16, 16, 70 ), 5, Color( 255, 0, 0 ) )
+        debugoverlay.Box(location, Vector(-16, -16, 0), Vector(16, 16, 70), 5, Color(255, 0, 0))
 
         if not tr.Hit then
             local walker = ents.Create(GW_WALKER_CLASS)
-            if not IsValid( walker ) then break end
-            walker:SetPos( location )
+            if not IsValid(walker) then break end
+            walker:SetPos(location)
             walker:Spawn()
             walker:Activate()
-            table.insert( walkers, walker )
-            SafeRemoveEntityDelayed( walker, 12 )
-            decoyCount = decoyCount + 1
+            table.insert(walkers, walker)
+            SafeRemoveEntityDelayed(walker, self.AbilityDuration)
+            spawnedCount = spawnedCount + 1
         end
 
     end
 
     if #walkers >= 2 then
         local swap = walkers[ math.random(1, #walkers) ]
-        local spos = swap:GetPos() + Vector( 0, 0, 2 )
-        swap:SetPos( self.Owner:GetPos() + Vector( 0, 0, 2 ) )
-        self.Owner:SetPos( spos )
-        self.Owner:SetModel( GAMEMODE.GWConfig.HidingModels[ math.random( 1, #GAMEMODE.GWConfig.HidingModels ) ] )
+        local spos = swap:GetPos() + Vector(0, 0, 2)
+        swap:SetPos(self.Owner:GetPos() + Vector(0, 0, 2))
+        self.Owner:SetPos(spos)
+        self.Owner:SetModel(GAMEMODE.GWConfig.HidingModels[ math.random(1, #GAMEMODE.GWConfig.HidingModels) ])
     else
-        self.Owner:SetHealth( 100 )
+        self.Owner:SetHealth(100)
     end
 
 end
