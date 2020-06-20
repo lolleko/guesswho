89a7cda0dbf8f5698b5289f0d5218fee8b64ba63
lolleko <lorenz.junglas@student.kit.edu>
2020-06-20 23:49:16 +0200

    Updated Blast off to new SWEP base

---
+++ gamemodes/guesswho/entities/effects/gw_blast_off.lua
@@ -0,0 +1,30 @@
+function EFFECT:Init(data)
+    self.Owner = data:GetEntity()
+    self.Duration = data:GetMagnitude()
+    self.FadeoutTime = 3
+    self.EndTime = CurTime() + self.Duration + self.FadeoutTime
+    self.Emitter = ParticleEmitter(self.Owner:GetPos())
+end
+
+function EFFECT:Think()
+    if self.EndTime < CurTime() then
+        self.Emitter:Finish()
+        return false
+    end
+
+    local vOffset = self.Owner:GetPos() + Vector(math.Rand(-6, 6), math.Rand(-6, 6), math.Rand(-6, 6))
+    local vNormal = (vOffset - self.Owner:GetPos()):GetNormalized()
+
+    local particle = self.Emitter:Add("particles/smokey", vOffset)
+    if not particle then return end
+
+    particle:SetVelocity(vNormal * math.Rand( 10, 30 ) + VectorRand() * 40)
+    particle:SetDieTime(3.0)
+    particle:SetStartAlpha(math.Rand(50, 150))
+    particle:SetStartSize(math.Rand(48, 64 ))
+    particle:SetEndSize(math.Rand(128, 312 ))
+    particle:SetRoll(math.Rand(-0.2, 0.2 ))
+    particle:SetColor(200, 200, 210)
+
+    return true
+end
--- gamemodes/guesswho/entities/weapons/weapon_gw_blasting_off.lua
+++ gamemodes/guesswho/entities/weapons/weapon_gw_blasting_off.lua
@@ -1,79 +1,30 @@
 AddCSLuaFile()
 
 SWEP.Base = "weapon_gwbase"
-SWEP.Name = "Blasting off (again)"
+SWEP.Name = "Blast Off"
+
+SWEP.AbilityDuration = 3
+SWEP.AbilityDescription = "Launches all seekers into the air after a short delay.\n The seekers are stuck in the air for at least $AbilityDuration seconds.\n\nDoes not work well indoors."
 
 function SWEP:Ability()
-    self.LaunchedEnts = {}
+    if SERVER then
+        for _, ply in pairs(player.GetAll()) do
+            if ply:IsAlive() and ply:IsSeeking() then
+                timer.Simple(0.5, function() ply:SetVelocity(Vector(0, 0, 2500)) end)
+                local effect = EffectData()
+                effect:SetEntity(ply)
+                effect:SetMagnitude(3)
+                util.Effect("gw_blast_off", effect, true, true)
 
-    for _,v in pairs(player.GetAll()) do
-        if v:IsSeeking() then
-            v:SetVelocity(Vector(0, 0, 5000))
-            table.insert(self.LaunchedEnts, v)
-            local tName = "gwLaunch" .. v:SteamID()
-            if timer.Exists(tName) then
-                timer.Remove(tName)
-                v:SetGravity(1)
+                local tName = "gwLaunch" .. ply:SteamID()
+                timer.Create(tName, 0.1, 15, function()
+                    if util.QuickTrace(ply:EyePos(), Vector(0, 0, 30), ply).HitWorld then
+                        timer.Remove(tName)
+                        ply:SetGravity(-1)
+                        timer.Simple(self.AbilityDuration, function() ply:SetGravity(1) end)
+                    end
+                end)
             end
-            timer.Create(tName, 0.1, 15, function()
-                if util.QuickTrace(v:EyePos(), Vector(0, 0, 30), v).HitWorld then
-                    timer.Remove(tName)
-                    v:SetGravity(-1)
-                    timer.Simple(3, function() v:SetGravity(1) end)
-                end
-            end)
-        end
-    end
-
-end
-
-if CLIENT then
-    local function GetEmitter(self, Pos)
-        if ( self.Emitter ) then
-            if self.EmitterTime > CurTime() then
-                return self.Emitter
-            else
-                self.Emitter:Finish()
-                self.Emitter = nil
-            end
-        end
-
-        self.Emitter = ParticleEmitter(Pos)
-        self.EmitterTime = CurTime() + 10
-        return self.Emitter
-
-    end
-
-    local function Trail(self)
-        self.SmokeTimer = self.SmokeTimer or 0
-        if ( self.SmokeTimer > CurTime() ) then return end
-
-        self.SmokeTimer = CurTime() + 0.05
-
-        local vOffset = self:GetPos() + Vector( math.Rand( -3, 3 ), math.Rand( -3, 3 ), math.Rand( -3, 3 ) )
-        local vNormal = ( vOffset - self:GetPos() ):GetNormalized()
-
-        local emitter = GetEmitter( self, vOffset )
-
-        local particle = emitter:Add( "particles/smokey", vOffset )
-        if ( not particle ) then return end
-
-        particle:SetVelocity( vNormal * math.Rand( 10, 30 ) )
-        particle:SetDieTime( 3.0 )
-        particle:SetStartAlpha( math.Rand( 50, 150 ) )
-        particle:SetStartSize( math.Rand( 32, 64 ) )
-        particle:SetEndSize( math.Rand( 128, 312 ) )
-        particle:SetRoll( math.Rand( -0.2, 0.2 ) )
-        particle:SetColor( 200, 200, 210 )
-    end
-
-
-    function SWEP:Think()
-        if self.LaunchedEnts then
-            for k,v in pairs(self.LaunchedEnts) do
-                Trail(v)
-            end
-            timer.Simple(4.5, function() self.LaunchedEnts = nil end)
         end
     end
 end
